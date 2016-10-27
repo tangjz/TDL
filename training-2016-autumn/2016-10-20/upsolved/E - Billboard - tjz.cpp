1cef5ec0e544d99911d47393611c9a97532a733d
tangjz <t251346744@gmail.com>
2016-10-27 14:43:43 +0800

    2015 ACM-ICPC Asia Tehran Regional Contest

--- training-2016-autumn/2016-10-20/upsolved/E - Billboard - tjz.cpp
+++ training-2016-autumn/2016-10-20/upsolved/E - Billboard - tjz.cpp
@@ -52,38 +52,36 @@ int chk(int L, int R, int tot)
 }
 int main()
 {
-	while(scanf("%d%d%d%d", &n, &m, &r, &s) == 4 && n + m + r + s)
+	while(scanf("%d%d%d%d", &m, &n, &r, &s) == 4 && m + n + r + s)
 	{
-		for(int i = 0; i < n; ++i)
-			for(int j = 0; j < m; ++j)
+		for(int j = 1; j <= m; ++j)
+			for(int i = 1; i <= n; ++i)
+			{
 				scanf("%d", f[i] + j);
+				f[i][j] = f[i - 1][j] + !f[i][j];
+			}
 		ans = 0;
-		for(int i = 0; i < n; ++i)
+		for(int i = 1; i <= m; ++i)
 			ord[i] = i;
-		for(int L = 0; L < m; ++L)
-		{
-			memset(g, 0, n * sizeof(int));
-			for(int R = L; R < m; ++ R)
+		for(int len = n; len >= 1 && ans < m * len; --len)
+			for(int L = 0, R = len; R <= n && ans < m * len; ++L, ++R)
 			{
 				int mx = 0;
-				for(int i = 0; i < n; ++i)
-					g[i] += !f[i][R];
-				if(ans >= n * (R - L + 1))
-					continue;
-				sort(ord, ord + n, cmp);
-				for(int i = 0; i < n; ++i)
+				for(int i = 1; i <= m; ++i)
+					g[i] = f[R][i] - f[L][i];
+				sort(ord + 1, ord + m + 1, cmp);
+				for(int i = 1; i <= m; ++i)
 					pos[ord[i]] = i;
-				memset(seg, 0, (n << 1) * sizeof(Segment));
-				for(int i = 0, j = 0; i < n; ++i)
+				memset(seg, 0, (m + 1 << 1) * sizeof(Segment));
+				for(int i = 1, j = 1; i <= m; ++i)
 				{
-					for(upd(0, n - 1, i, 1); j + r <= i && chk(0, n - 1, r) > s; upd(0, n - 1, j++, -1));
+					for(upd(1, m, i, 1); j + r <= i && chk(1, m, r) > s; upd(1, m, j++, -1));
 					if(mx < i - j + 1)
 						mx = i - j + 1;
 				}
-				if(ans < mx * (R - L + 1))
-					ans = mx * (R - L + 1);
+				if(ans < mx * len)
+					ans = mx * len;
 			}
-		}
 		printf("%d\n", ans);
 	}
 	return 0;
