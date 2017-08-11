fa5122fa15af5c63b0605ccc0f285af7e10d621c
Elizur Taylor <t251346744@gmail.com>
2017-08-12 04:07:58 +0800

    bug fixed

--- training-2016-autumn/2016-07-21/L - La Vie en rose - tjz.cpp
+++ training-2016-autumn/2016-07-21/L - La Vie en rose - tjz.cpp
@@ -4,37 +4,29 @@
 using namespace std;
 const int maxn = 100000, maxm = 5000, maxd = 26;
 int t, n, m;
-bitset<maxm> pos[maxd][3], f[3];
-char str[maxn + 10], pat[maxm + 10];
-int main()
-{
+bitset<maxn + 1> pos[maxd + 1], f[3];
+char str[maxn + 3], pat[maxm + 3];
+int main() {
 	scanf("%d", &t);
-	while(t--)
-	{
+	while(t--) {
 		scanf("%d%d%s%s", &n, &m, str + 1, pat + 1);
 		for(int i = 0; i < maxd; ++i)
-			pos[i][0].reset();
-		for(int i = 1; i <= m; ++i)
-		{
-			int ch = pat[i] - 'a';
-			pos[ch][0].set(i);
-		}
-		for(int i = 0; i < maxd; ++i)
-			for(int j = 1; j < 3; ++j)
-				pos[i][j] = pos[i][j - 1] >> 1;
-		for(int i = 0; i < 3; ++i)
-			f[i].reset();
-		f[0].set(0);
-		for(int i = 1, cur = 1, nxt = 2; i <= n; ++i, swap(cur, nxt))
-		{
+			pos[i].reset();
+		for(int i = 1; i <= n; ++i) {
 			int ch = str[i] - 'a';
-			f[nxt] = (f[0] & pos[ch][2]) << 1;
-			f[0] = ((f[0] & pos[ch][1]) | (f[cur] & pos[ch][0])) << 1;
-			str[i - m + 1] = f[0].test(m) ? '1' : '0';
-			f[0].set(0);
+			pos[ch].set(i);
 		}
-		for(int i = n - m + 2; i <= n; ++i)
-			str[i] = '0';
+		pat[0] = pat[m + 1] = 'a' + maxd;
+		f[0].set();
+		f[1].reset();
+		int cur = 1, nxt = 2;
+		for(int i = 1; i <= m; ++i, swap(cur, nxt)) {
+			f[nxt] = (f[0] << 1) & pos[pat[i + 1] - 'a'];
+			f[0] = ((f[0] << 1) & pos[pat[i] - 'a']) | ((f[cur] << 1) & pos[pat[i - 1] - 'a']);
+		}
+		f[0] |= f[cur];
+		for(int i = 1; i <= n; ++i)
+			str[i] = '0' + (i + m - 1 <= n && f[0].test(i + m - 1));
 		puts(str + 1);
 	}
 	return 0;
